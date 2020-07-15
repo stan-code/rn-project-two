dbf6582ed996d8cebdd15b78ca4f9eb48e5e2e8d
Stan-Gogan <sjgogan@me.com>
2020-07-15 17:05:47 +1200

    style input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import {View, StyleSheet, Text, TextInput, Button} from 'react-native'
+import {View, StyleSheet, Text, Button} from 'react-native'
+
+
 import Card from '../components/Card'
+import Input from '../components/Input'
+import  Colors from '../constants/colors'
 
 
 const StartGameScreen = props => {
-  
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
         <Card style={styles.inputContainer}>
           <Text >Select a Number</Text>
-          <TextInput/>
+          <Input style={styles.input}/>
         <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title="Reset" color="#c7171c" onPress={() => console.log("reset")} /></View>
-          <View style={styles.button}><Button title="Confirm" color="#f7287b" onPress={() => console.log("confirm")} /></View>
+          <View style={styles.button}>
+            <Button title="Reset" color={Colors.primary} onPress={() => console.log("reset")} />
+            </View>
+          <View style={styles.button}>
+            <Button title="Confirm" color={Colors.accent}  onPress={() => console.log("confirm")} />
+            </View>
         </View>
       </Card>
     </View>
@@ -43,8 +51,11 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
   }
-
 })
 
 export default StartGameScreen
